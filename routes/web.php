7356dcd5d245afdd25f14f16612de2a2df7f1045
Aiman Daniel <aiman2301@gmail.com>
2019-03-02 18:02:52 +0800

    Remove junk

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,5 @@
 <?php
 
-use App\Branch;
-use App\Customer;
-use App\Order;
-use Illuminate\Http\Request;
-
 Route::get('/', 'Auth\LoginController@showLoginForm')
     ->name('login')
     ->middleware('guest');
@@ -24,7 +19,6 @@ Route::middleware('auth')->group(function () {
     Route::resources([
         'customers' => 'CustomerController',
         'orders' => 'OrderController',
-        'inventories' => 'InventoryController',
         'users' => 'UserController',
         'branches' => 'BranchController',
         'products' => 'ProductController',
@@ -35,39 +29,12 @@ Route::middleware('auth')->group(function () {
 
     Route::get('orders/{order}/generate_receipt', 'OrderController@generateReceipt')->name('orders.generateReceipt');
 
-    /*Route::prefix('api')->group(function () {
-        Route::get('customers', function () {
-            return datatables()->of(Customer::latest()->get())->toJson();
-        });
-
-        Route::get('orders', function () {
-            return datatables()->of(Order::latest()->get())->toJson();
-        });
-
-        Route::get('orders/{id}/customer', function ($id) {
-            $order = Order::find($id);
-            $customer = $order->customer()->get();
-
-            return datatables()->of($customer)->toJson();
-        });
-
-        Route::get('branches', function () {
-            return datatables()->of(Branch::orderBy('id', 'desc')->get())->toJson();
-        });
-    });*/
-
-    // Route::get('carts', 'CartController@index')->name('carts.index');
     Route::get('carts/{customer}', 'CartController@viewCartByCustomer')->name('carts.viewByCustomer');
     
-    Route::post('products/{product}/add_to_cart', 'ProductController@addToCart')->name('products.addToCart');
+    Route::post('products/{product}/add_to_cart', 'CartController@addToCart')->name('carts.addToCart');
 
-    Route::get('checkout/{customer}', 'CartController@checkout')->name('checkout');
+    Route::get('checkout/{customer}', 'CheckoutController@checkout')->name('checkout');
 
     Route::get('logout', 'Auth\LoginController@logout')->name('logout');
 
-});
-
-Route::get('/test', function (Request $request) {
-
-    dd(session('test'), session('customer_id'), $request->session()->all());
 });
