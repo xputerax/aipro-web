dfa8344b89c88dfb3d1594bc479ba2642dce1f26
Aiman Daniel <aiman2301@gmail.com>
2019-03-03 16:46:09 +0800

    Add order edit page

---
+++ resources/views/order/edit.blade.php
@@ -0,0 +1,92 @@
+@extends('backend-layout')
+
+@php
+    $parse_body_tag = false
+@endphp
+
+@section('title')
+Editing Order #{{ $order->id }}
+@endsection
+
+@section('content-1')
+<div class="x_panel">
+    <form action="{{ route('orders.update', compact('order')) }}" method="post" class="form-horizontal form-label-left">
+        @csrf
+        @method('PUT')
+
+        <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="branch">
+                Branch
+            </label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <p class="form-control-static">{{ $order->branch->name }}</p>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="customer">
+                Customer
+            </label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <p class="form-control-static">{{ $order->customer->full_name }}</p>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="checkout">
+                Checkout By
+            </label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <p class="form-control-static">{{ $order->checkout_user->full_name }}</p>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="resolved">
+                Resolved By
+            </label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <p class="form-control-static">{{ $order->resolve_user->full_name ?? '-' }}</p>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="delivered">
+                Delivered By
+            </label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <p class="form-control-static">{{ $order->delivery_user->full_name ?? '-' }}</p>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="status">
+                Status <span class="required">*</span>
+            </label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <select name="status" class="form-control">
+                    <option value="pending" {{ $order->status == 'pending' ? 'selected' : '' }}>Pending</option>
+                    <option value="resolved" {{ $order->status == 'resolved' ? 'selected' : '' }}>Resolved</option>
+                    <option value="delivered"{{ $order->status == 'delivered' ? 'selected' : '' }}>Delivered</option>
+                </select>
+            </div>
+        </div>
+
+        <div class="form-group">
+            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="deposit">
+                Deposit <span class="required">*</span>
+            </label>
+            <div class="col-md-6 col-sm-6 col-xs-12">
+                <input type="text" class="form-control" name="deposit" value="{{ $order->deposit }}">
+            </div>
+        </div>
+
+        <div class="form-group">
+            <div class="col-md-6 col-sm-6 col-xs-12 col-md-offset-3 col-sm-offset-3">
+                <input type="submit" value="Save" class="btn btn-primary">
+                <a href="{{ route('orders.show', compact('order')) }}" class="btn btn-primary">View</a>
+            </div>
+        </div>
+    </form>
+</div>
+@endsection
