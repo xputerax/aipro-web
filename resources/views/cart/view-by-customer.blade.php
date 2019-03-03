17bb1927f8a2f755fd71e542cea31f6c6e3429c5
Aiman Daniel <aiman2301@gmail.com>
2019-03-03 23:49:30 +0800

    Add change quantity form

--- resources/views/cart/view-by-customer.blade.php
+++ resources/views/cart/view-by-customer.blade.php
@@ -24,22 +24,36 @@ Viewing Customer Cart: {{ $customer->full_name }}
                             <th>Total Price</th>
                         </tr>
                     </thead>
-    
+
                     <tbody>
                     @if($customer->carts)
                         @foreach($customer->carts as $cart)
-                        
+
                         @php
                             $total_price += $cart->total_price;
                         @endphp
 
-                        <tr>
+                        <tr data-cart-id="{{ $cart->id }}">
                             <td>
                                 <a href="{{ route('products.show', ['product' => $cart->product]) }}">
                                 {{ $cart->product->name }}
                                 </a>
                             </td>
-                            <td>{{ $cart->quantity }}</td>
+                            <td>
+                                <a href="#" id="cart_{{ $cart->id }}_link" class="show">
+                                    {{ $cart->quantity }}
+                                </a>
+                                <div id="cart_{{ $cart->id }}_form" class="hidden">
+                                    <form action="{{ route('carts.modify', compact('cart')) }}" method="post" class="form-inline">
+                                        @csrf
+                                        @method('PUT')
+
+                                        <input type="text" name="newQuantity" class="form-control" value="{{ $cart->quantity }}" style="width: 20%">
+                                        <input type="submit" value="Save" class="btn btn-primary">
+                                        <a href="#" id="cart_{{ $cart->id }}_cancel" class="btn btn-danger">Cancel</a>
+                                    </form>
+                                </div>
+                            </td>
                             <td>{{ $cart->price }}</td>
                             <td>{{ $cart->total_price }}</td>
                         </tr>
@@ -69,7 +83,40 @@ Viewing Customer Cart: {{ $customer->full_name }}
                 <a href="{{ route('checkout', compact('customer')) }}" class="btn btn-primary">Checkout</a>
             </div>
         </div>
-    
+
     </div> <!-- end .x_content -->
 </div> <!-- end .x_panel -->
+@endsection
+
+@section('scripts')
+@parent
+
+<script>
+$('a[id$=_link]').click(function (e) {
+
+    let tr = $(this).parent().parent();
+    let cart_id = tr.data('cart-id');
+
+    // hide link
+    $(this).toggleClass('show').toggleClass('hidden');
+
+    // show form
+    $('div#cart_' + cart_id + '_form').toggleClass('hidden');
+
+});
+
+$('a[id$=_cancel]').click(function (e) {
+
+    let tr = $(this).parent().parent().parent().parent();
+    let cart_id = tr.data('cart-id');
+    let form = $('div#cart_' + cart_id + '_form');
+    let link = $('a#cart_' + cart_id + '_link');
+
+
+    form.toggleClass('hidden');
+    link.toggleClass('hidden').toggleClass('show');
+
+    console.log(link)
+});
+</script>
 @endsection
