4164ff78d66f557cd2d78b8a58f7ba0910de6300
Aiman Daniel <aiman2301@gmail.com>
2019-03-14 07:54:11 +0800

    Add sales report feature
    - Remove 'this week' column from dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -3,41 +3,27 @@
 @section('title', 'Dashboard')
 
 @php
-    $parse_body_tag = false;    
+    $parse_body_tag = false;
 @endphp
 
 @section('content-1')
-{{-- <div class="right_col" role="main">
-    <div class="">
-        <div class="page-title">
-            <div class="title_left">
-                <h3>Dashboard</h3>
-            </div>
-        </div>
-
-        <div class="clearfix"></div>
-
-        <div class="row">
-            <div class="col-md-12 col-sm-12 col-xs-12"> --}}
-                <div class="x_panel">
-                    <div class="x_title">
-                        <h2>Dashboard</h2>
-                        <div class="clearfix"></div>
-                    </div>
-                    <div class="x_content">
-                        test
-                    </div> <!-- end .x_content -->
-
-                    <div class="x_title">
-                        <h2>test</h2>
-                        <div class="clearfix"></div>
-                    </div> <!-- end .x_title -->
-                    <div class="x_content">
-                        <h2></h2>
-                    </div> <!-- end .x_content -->
-                </div> <!-- end .x_panel -->
-            {{-- </div> <!-- end column -->
-        </div> <!-- end .row -->
+<div class="row tile_count">
+    <div class="col-md-4 tile_stats_count">
+        <span class="count_top"><i class="fa fa-money"></i> Today</span>
+        <div class="count">RM {{ $sales['day'] }}</div>
+        <span class="count_bottom"></span>
     </div>
-</div> <!-- end .right_col --> --}}
+
+    <div class="col-md-4 tile_stats_count">
+        <span class="count_top"><i class="fa fa-money"></i> This Month</span>
+        <div class="count">RM {{ $sales['month'] }}</div>
+        <span class="count_bottom"></span>
+    </div>
+
+    <div class="col-md-4 tile_stats_count">
+        <span class="count_top"><i class="fa fa-money"></i> This Year</span>
+        <div class="count">RM {{ $sales['year'] }}</div>
+        <span class="count_bottom"></span>
+    </div>
+</div> <!-- end .row .tile_count -->
 @endsection
