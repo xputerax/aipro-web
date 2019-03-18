580e7ac5cd957a4bce442b5881a9aad56bd400f8
Aiman Daniel <aiman2301@gmail.com>
2019-03-18 18:36:41 +0800

    Add get-brands-all-branches gate

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -194,5 +194,9 @@ class AuthServiceProvider extends ServiceProvider
             return $user->group_id === self::AIPRO_CEO_GROUP_ID;
         });
 
+        Gate::define('get-brands-all-branches', function($user) {
+            return $user->group_id === self::AIPRO_CEO_GROUP_ID;
+        });
+
     }
 }
