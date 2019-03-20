fe467971714a4c31dede1cb1b63e1ee801ac5298
Aiman Daniel <aiman2301@gmail.com>
2019-03-20 23:36:17 +0800

    Add description column
    - change javascript for description and quantity
    - remove junk

--- resources/views/cart/view-by-customer.blade.php
+++ resources/views/cart/view-by-customer.blade.php
@@ -10,116 +10,148 @@ Viewing Customer Cart: {{ $customer->full_name }}
 @endsection
 
 @section('content-1')
-<div class="x_panel">
-    <div class="x_content">
+<div class="row">
+    <div class="col-md-12">
+        <table class="table table-bordered table-striped">
+            <thead>
+                <tr>
+                    <th>Product</th>
+                    <th>Description</th>
+                    <th>Quantity</th>
+                    <th>Price/Qty</th>
+                    <th>Total Price</th>
+                </tr>
+            </thead>
 
-        <div class="row">
-            <div class="col-md-12">
-                <table class="table table-bordered table-striped">
-                    <thead>
-                        <tr>
-                            <th>Product</th>
-                            <th>Quantity</th>
-                            <th>Price/Qty</th>
-                            <th>Total Price</th>
-                        </tr>
-                    </thead>
+            <tbody>
+            @if($customer->carts)
+                @foreach($customer->carts as $cart)
 
-                    <tbody>
-                    @if($customer->carts)
-                        @foreach($customer->carts as $cart)
+                @php
+                    $total_price += $cart->total_price;
+                @endphp
 
-                        @php
-                            $total_price += $cart->total_price;
-                        @endphp
+                <tr data-cart-id="{{ $cart->id }}">
+                    <td>
+                        <a href="{{ route('products.show', ['product' => $cart->product]) }}">
+                        {{ $cart->product->name }}
+                        </a>
+                    </td>
 
-                        <tr data-cart-id="{{ $cart->id }}">
-                            <td>
-                                <a href="{{ route('products.show', ['product' => $cart->product]) }}">
-                                {{ $cart->product->name }}
-                                </a>
-                            </td>
+                    <td>
+                        <a href="#" id="cart_{{ $cart->id }}_description_link" data-cart-id="{{ $cart->id }}">
 
-                            <td>
-                                <a href="#" id="cart_{{ $cart->id }}_link" class="show">
-                                    {{ $cart->quantity }}
-                                </a>
-                                <div id="cart_{{ $cart->id }}_form" class="hidden">
-                                    <form action="{{ route('carts.modify', compact('cart')) }}" method="post" class="form-inline">
-                                        @csrf
-                                        @method('PUT')
-
-                                        <input type="text" name="quantity" class="form-control" value="{{ $cart->quantity }}" style="width: 20%">
-                                        <input type="submit" value="Save" class="btn btn-primary">
-                                        <a href="#" id="cart_{{ $cart->id }}_cancel" class="btn btn-danger">Cancel</a>
-                                    </form>
-                                </div>
-                            </td>
-
-                            <td>{{ $cart->price }}</td>
-
-                            <td>{{ $cart->total_price }}</td>
-                        </tr>
-                        @endforeach
-
-                        @php
-                            $total_price = sprintf("%.2f", $total_price);
-                        @endphp
-
-                        <tr>
-                            <td colspan="2"></td>
-                            <td>Total:</td>
-                            <td>{{ $total_price }}</td>
-                        </tr>
+                    @if(isset($cart->description))
+                        <pre>{{ $cart->description }}</pre>
                     @else
-                        <tr>
-                            <td colspan="4">No Item</td>
-                        </tr>
+                        Add Description
                     @endif
-                    </tbody>
-                </table>
-            </div> <!-- end .col-md-12 -->
-        </div> <!-- end .row -->
 
-        <div class="row">
-            <div class="col-md-12">
-                <a href="{{ route('checkout', compact('customer')) }}" class="btn btn-primary">Checkout</a>
-            </div>
-        </div>
+                        </a>
 
-    </div> <!-- end .x_content -->
-</div> <!-- end .x_panel -->
+                        <div id="cart_{{ $cart->id }}_description_form" class="hidden">
+                            <form action="{{ route('carts.modifyDescription', compact('cart')) }}" method="post">
+                                @csrf
+                                @method('PUT')
+
+                                <textarea name="description" cols="10" rows="5" class="form-control">{{ $cart->description }}</textarea>
+
+                                <br>
+
+                                <input type="submit" value="Save" class="btn btn-primary">
+                                <a href="#" class="btn btn-danger"
+                                id="cart_{{ $cart->id }}_description_cancel"
+                                data-cart-id="{{ $cart->id }}">Cancel</a>
+                            </form>
+                        </div> <!-- end add description form -->
+                    </td> <!-- end description column -->
+
+                    <td>
+                        <a href="#" id="cart_{{ $cart->id }}_quantity_link" data-cart-id="{{ $cart->id }}">
+                            {{ $cart->quantity }}
+                        </a>
+                        <div id="cart_{{ $cart->id }}_quantity_form" class="hidden">
+                            <form action="{{ route('carts.modifyQuantity', compact('cart')) }}" method="post" class="form-inline">
+                                @csrf
+                                @method('PUT')
+
+                                <input type="text" name="quantity" class="form-control" value="{{ $cart->quantity }}" style="width: 20%">
+                                <input type="submit" value="Save" class="btn btn-primary">
+                                <a href="#" id="cart_{{ $cart->id }}_quantity_cancel" class="btn btn-danger" data-cart-id="{{ $cart->id }}">Cancel</a>
+                            </form>
+                        </div> <!-- end quantity form -->
+                    </td> <!-- end quantity column -->
+
+                    <td>{{ $cart->price }}</td>
+
+                    <td>{{ $cart->total_price }}</td>
+                </tr>
+                @endforeach
+
+                @php
+                    $total_price = sprintf("%.2f", $total_price);
+                @endphp
+
+                <tr>
+                    <td colspan="3"></td>
+                    <td>Total:</td>
+                    <td>{{ $total_price }}</td>
+                </tr>
+            @else
+                <tr>
+                    <td colspan="5">No Item</td>
+                </tr>
+            @endif
+            </tbody>
+        </table>
+    </div> <!-- end .col-md-12 -->
+</div> <!-- end .row -->
+
+<div class="row">
+    <div class="col-md-12">
+        <a href="{{ route('checkout', compact('customer')) }}" class="btn btn-primary">Checkout</a>
+    </div>
+</div>
 @endsection
 
 @section('scripts')
 @parent
 
 <script>
-$('a[id$=_link]').click(function (e) {
-
-    let tr = $(this).parent().parent();
-    let cart_id = tr.data('cart-id');
-
-    // hide link
-    $(this).toggleClass('show').toggleClass('hidden');
-
-    // show form
-    $('div#cart_' + cart_id + '_form').toggleClass('hidden');
-
-});
-
-$('a[id$=_cancel]').click(function (e) {
-
-    let tr = $(this).parent().parent().parent().parent();
-    let cart_id = tr.data('cart-id');
-    let form = $('div#cart_' + cart_id + '_form');
-    let link = $('a#cart_' + cart_id + '_link');
-
+$("a[id$='_description_link']").click(function (e) {
+    let anchor = $(this);
+    let cart_id = anchor.data('cart-id');
+    let form = $("div#cart_" + cart_id + "_description_form");
 
     form.toggleClass('hidden');
-    link.toggleClass('hidden').toggleClass('show');
+    anchor.toggleClass('hidden');
+});
 
-    console.log(link)
+$("a[id$='_description_cancel']").click(function (e) {
+    let cart_id = $(this).data('cart-id');
+    let anchor = $("a#cart_" + cart_id + "_description_link");
+    let form = $(this).parent().parent();
+
+    anchor.toggleClass('hidden');
+    form.toggleClass('hidden');
+});
+
+$("a[id$='_quantity_link']").click(function (e) {
+    let anchor = $(this);
+    let cart_id = anchor.data('cart-id');
+    let form = $("div#cart_" + cart_id + "_quantity_form");
+
+    anchor.toggleClass('hidden');
+    form.toggleClass('hidden');
+});
+
+$("a[id$='_quantity_cancel']").click(function (e) {
+    let cart_id = $(this).data('cart-id');
+    let anchor = $("a#cart_" + cart_id + "_quantity_link");
+    let form = $(this).parent().parent();
+
+    form.toggleClass('hidden');
+    anchor.toggleClass('hidden');
 });
 </script>
 @endsection
