1e7d314edd38464da4ae509adc07a8da8c50d083
Aiman Daniel <aiman2301@gmail.com>
2019-03-21 18:02:29 +0800

    Define deselect customer page and remove junk

--- resources/views/customer/deselect.blade.php
+++ resources/views/customer/deselect.blade.php
@@ -7,8 +7,15 @@
 @endphp
 
 @section('content-1')
-<div class="x_panel">
-    <div class="x_content">
-    </div>
-</div>
+
+@if(isset($customer))
+<form action="{{ route('customers.deselect') }}" method="get">
+    <h4>Deselect customer {{ $customer->full_name }}</h4>
+    <input type="submit" value="Confirm Deselect" class="btn btn-primary">
+</form>
+@else
+<h4>
+    No customer selected!
+</h4>
+@endif
 @endsection
