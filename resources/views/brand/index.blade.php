80194572dff0f24d3b04eedfe694c1d179b85641
Aiman Daniel <aiman2301@gmail.com>
2019-03-23 11:35:41 +0800

    Remove junk

--- resources/views/brand/index.blade.php
+++ resources/views/brand/index.blade.php
@@ -7,43 +7,39 @@
 @endphp
 
 @section('content-1')
-<div class="x_panel">
-    <div class="x_content">
-        <table class="table table-bordered table-striped" id="brand_table">
-            <thead>
-                <tr>
-                    <th>ID</th>
-                    <th>Name</th>
-                    <th>Description</th>
-                    @can('get-brands-all-branches')
-                    <th>Branch</th>
-                    @endcan
-                </tr>
-            </thead>
+<table class="table table-bordered table-striped" id="brand_table">
+    <thead>
+        <tr>
+            <th>ID</th>
+            <th>Name</th>
+            <th>Description</th>
+            @can('get-brands-all-branches')
+            <th>Branch</th>
+            @endcan
+        </tr>
+    </thead>
 
-            <tbody>
-            @if($brands->count())
-            @foreach($brands as $brand)
-                <tr>
-                    <td>{{ $brand->id }}</td>
-                    <td>
-                        <a href="{{ route('brands.show', compact('brand')) }}">{{ $brand->name }}</a>
-                    </td>
-                    <td>{{ $brand->description ?? '-' }}</td>   
-                    @can('get-brands-all-branches')
-                    <td>{{ $brand->branch->name }}</td>
-                    @endcan
-                </tr>    
-            @endforeach
-            @else
-                <tr>
-                    <td colspan="2">No Data</td>
-                </tr>
-            @endif
-            </tbody>
-        </table>
-    </div>
-</div>
+    <tbody>
+    @if($brands->count())
+    @foreach($brands as $brand)
+        <tr>
+            <td>{{ $brand->id }}</td>
+            <td>
+                <a href="{{ route('brands.show', compact('brand')) }}">{{ $brand->name }}</a>
+            </td>
+            <td>{{ $brand->description ?? '-' }}</td>
+            @can('get-brands-all-branches')
+            <td>{{ $brand->branch->name }}</td>
+            @endcan
+        </tr>
+    @endforeach
+    @else
+        <tr>
+            <td colspan="2">No Data</td>
+        </tr>
+    @endif
+    </tbody>
+</table>
 @endsection
 
 @section('scripts')
--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -7,68 +7,64 @@
 @endphp
 
 @section('content-1')
-<div class="x_panel">
-    <div class="x_content">
-        <table class="table table-bordered table-striped" id="product_table">
-            <thead>
-                <tr>
-                    <th>Name</th>
-                    <th>Brand</th>
-                    <th>Min. Price</th>
-                    <th>Max. Price</th>
-                    <th>Stock</th>
-                    <th>Type</th>
-                    @if(session('customer'))
-                    <th>Action</th>
-                    @endif
-                </tr>
-            </thead>
-
-            <tbody>
-            @if($products->count())
-            @foreach($products as $product)
-                <tr>
-                    <td>
-                        <a href="{{ route('products.edit', compact('product')) }}">{{ $product->name }}</a>
-                    </td>
-                    <td>
-                        <a href="{{ route('brands.show', ['brand'=>$product->brand]) }}">{{ $product->brand->name }}</a>
-                    </td>
-                    <td>{{ $product->min_price }}</td>
-                    <td>{{ $product->max_price }}</td>
-                    <td>{{ $product->stock }}</td>
-                    <td>{{ $product->type }}</td>
-                    @if(session('customer'))
-                    <td>
-                        <form action="{{ route('carts.addToCart', compact('product')) }}" class="form-inline" method="post">
-                            @csrf
-                            <div class="container">
-                                <div class="row">
-                                    <div class="col-md-12 col-sm-12 col-xs-12 form-group">
-                                        <label for="price">Price</label>
-                                        <input type="text" name="price" value="{{ $product->min_price }}" style="width: 20%;" class="form-control">
-
-                                        <label for="quantity">Quantity</label>
-                                        <input type="text" name="quantity" value="1" style="width: 20%;" class="form-control">
-
-                                        <input type="submit" value="Add to Cart" class="btn btn-primary">
-                                    </div>
-                                </div>
-                            </div>
-                        </form>
-                    </td>
-                    @endif
-                </tr>
-            @endforeach
-            @else
-                <tr>
-                    <td colspan="7">No data</td>
-                </tr>
+<table class="table table-bordered table-striped" id="product_table">
+    <thead>
+        <tr>
+            <th>Name</th>
+            <th>Brand</th>
+            <th>Min. Price</th>
+            <th>Max. Price</th>
+            <th>Stock</th>
+            <th>Type</th>
+            @if(session('customer'))
+            <th>Action</th>
             @endif
-            </tbody>
-        </table>
-    </div>
-</div>
+        </tr>
+    </thead>
+
+    <tbody>
+    @if($products->count())
+    @foreach($products as $product)
+        <tr>
+            <td>
+                <a href="{{ route('products.edit', compact('product')) }}">{{ $product->name }}</a>
+            </td>
+            <td>
+                <a href="{{ route('brands.show', ['brand'=>$product->brand]) }}">{{ $product->brand->name }}</a>
+            </td>
+            <td>{{ $product->min_price }}</td>
+            <td>{{ $product->max_price }}</td>
+            <td>{{ $product->stock }}</td>
+            <td>{{ $product->type }}</td>
+            @if(session('customer'))
+            <td>
+                <form action="{{ route('carts.addToCart', compact('product')) }}" class="form-inline" method="post">
+                    @csrf
+                    <div class="container">
+                        <div class="row">
+                            <div class="col-md-12 col-sm-12 col-xs-12 form-group">
+                                <label for="price">Price</label>
+                                <input type="text" name="price" value="{{ $product->min_price }}" style="width: 20%;" class="form-control">
+
+                                <label for="quantity">Quantity</label>
+                                <input type="text" name="quantity" value="1" style="width: 20%;" class="form-control">
+
+                                <input type="submit" value="Add to Cart" class="btn btn-primary">
+                            </div>
+                        </div>
+                    </div>
+                </form>
+            </td>
+            @endif
+        </tr>
+    @endforeach
+    @else
+        <tr>
+            <td colspan="7">No data</td>
+        </tr>
+    @endif
+    </tbody>
+</table>
 
 @if(session('message'))
 <div class="alert alert-info">
