a867aa65fce0ae91313ff4633f3d59fc75b52d1e
Aiman Daniel <aiman2301@gmail.com>
2019-04-02 18:08:11 +0800

    Remove junk, follow new template

--- resources/views/brand/form.blade.php
+++ resources/views/brand/form.blade.php
@@ -8,108 +8,101 @@
     @section('breadcrumbs', Breadcrumbs::render('brand-create'))
 @endif
 
-@php
-    $parse_body_tag = false;
-@endphp
-
-@section('content-1')
+@section('content')
 @yield('breadcrumbs')
 
-<div class="x_panel">
-    <div class="x_content">
+@if(isset($brand))
+<form action="{{ route('brands.update', compact('brand')) }}" class="form-horizontal form-label-left" method="post">
+@method('PUT')
+@else
+<form action="{{ route('brands.store') }}" class="form-horizontal form-label-left" method="post">
+@endif
 
-        @if(isset($brand))
-        <form action="{{ route('brands.update', compact('brand')) }}" class="form-horizontal form-label-left" method="post">
-        @method('PUT')
-        @else
-        <form action="{{ route('brands.store') }}" class="form-horizontal form-label-left" method="post">
-        @endif
+@csrf
 
-        @csrf
+    <!-- start name section-->
+    <div class="form-group">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">
+            Name <span class="required">*</span>
+        </label>
 
-            <!-- start name section-->
-            <div class="form-group">
-                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">
-                    Name <span class="required">*</span>
-                </label>
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <input type="text" name="name" value="@if(isset($brand)){{ $brand->name }}@endif"
+                class="form-control col-md-7 col-xs-12" required>
+        </div>
+    </div>
+    <!-- end name section -->
 
-                <div class="col-md-6 col-sm-6 col-xs-12">
-                    <input type="text" name="name" value="@if(isset($brand)){{ $brand->name }}@endif" class="form-control col-md-7 col-xs-12" required>
-                </div>
-            </div>
-            <!-- end name section -->
+    @if(Auth::user()->can('add-brand-all-branches'))
+    <!-- start branch section -->
+    <div class="form-group">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">
+            Branch <span class="required">*</span>
+        </label>
 
-            @if(Auth::user()->can('add-brand-all-branches'))
-            <!-- start branch section -->
-            <div class="form-group">
-                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">
-                    Branch <span class="required">*</span>
-                </label>
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <select name="branch_id" id="branch_select" class="form-control">
+            @foreach($branches as $branch)
+                @php
+                    if(Route::currentRouteName() === "brands.edit" && $brand->branch_id === $branch->id) {
+                        $selected = "selected";
+                    } elseif(Route::currentRouteName() === "brands.create" && Auth::user()->branch_id === $branch->id) {
+                        $selected = "selected";
+                    } else {
+                        $selected = "";
+                    }
+                @endphp
+                <option value="{{ $branch->id }}" {{ $selected }}>
+                    {{ $branch->name }}
+                </option>
+            @endforeach
+            </select>
+        </div>
+    </div>
+    <!-- end branch section -->
+    @endif
 
-                <div class="col-md-6 col-sm-6 col-xs-12">
-                    <select name="branch_id" id="branch_select" class="form-control">
-                    @foreach($branches as $branch)
-                        @php
-                            if(Route::currentRouteName() === "brands.edit" && $brand->branch_id === $branch->id) {
-                                $selected = "selected";
-                            } elseif(Route::currentRouteName() === "brands.create" && Auth::user()->branch_id === $branch->id) {
-                                $selected = "selected";
-                            } else {
-                                $selected = "";
-                            }
-                        @endphp
-                        <option value="{{ $branch->id }}" {{ $selected }}>
-                            {{ $branch->name }}
-                        </option>
-                    @endforeach
-                    </select>
-                </div>
-            </div>
-            <!-- end branch section -->
-            @endif
+    <!-- start description section-->
+    <div class="form-group">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="description">
+            Description
+        </label>
 
-            <!-- start description section-->
-            <div class="form-group">
-                <label class="control-label col-md-3 col-sm-3 col-xs-12" for="description">
-                    Description
-                </label>
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <textarea name="description" cols="30" rows="10"
+                class="form-control col-md-7 col-xs-12">@if(isset($brand)){{ $brand->description }}@endif</textarea>
+        </div>
+    </div>
+    <!-- end description section -->
 
-                <div class="col-md-6 col-sm-6 col-xs-12">
-                    <textarea name="description" cols="30" rows="10" class="form-control col-md-7 col-xs-12">@if(isset($brand)){{ $brand->description }}@endif</textarea>
-                </div>
-            </div>
-            <!-- end description section -->
+    <!-- start button section-->
+    <div class="form-group">
+        <div class="col-md-offset-3 col-sm-offset-3 col-md-6 col-sm-6 col-xs-12">
+            <input type="submit" value="{{ isset($brand) ? 'Save' : 'Add Brand'}}" class="btn btn-primary">
+        </div>
+    </div>
+    <!-- end button section -->
 
-            <!-- start button section-->
-            <div class="form-group">
-                <div class="col-md-offset-3 col-sm-offset-3 col-md-6 col-sm-6 col-xs-12">
-                    <input type="submit" value="{{ isset($brand) ? 'Save' : 'Add Brand'}}" class="btn btn-primary">
-                </div>
-            </div>
-            <!-- end button section -->
+</form>
 
-        </form>
+@if($errors->any())
+@foreach($errors->all() as $error)
+{{ $error }}
+@endforeach
+@endif
 
-        @if($errors->any())
-        @foreach($errors->all() as $error)
-        {{ $error }}
-        @endforeach
-        @endif
-
-        @if(session('message'))
-        {{ session('message') }}
-        @endif
-    </div> <!-- end .x_content -->
-</div> <!-- end .x_panel -->
+@if(session('message'))
+{{ session('message') }}
+@endif
 @endsection
 
 @section('scripts')
 @parent
 
-@if(Auth::user()->can('add-brand-all-branches'))
+@can('add-brand-all-branches')
 <script>
     $("#branch_select").select2();
 </script>
-@endif
+@endcan
 
 @endsection
