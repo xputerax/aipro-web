74161a95e4e531b79e46f81fb0f6c8340ada4095
Aiman Daniel <aiman2301@gmail.com>
2019-04-02 18:02:24 +0800

    Remove junk, follow new template

--- resources/views/brand/index.blade.php
+++ resources/views/brand/index.blade.php
@@ -3,52 +3,51 @@
 @section('title', 'Brand List')
 @section('breadcrumbs', Breadcrumbs::render('brand-index'))
 
-@php
-    $parse_body_tag = false;
-@endphp
-
-@section('content-1')
+@section('content')
 @yield('breadcrumbs')
 
-<table class="table table-bordered table-striped" id="brand_table">
-    <thead>
-        <tr>
-            <th>Name</th>
-            <th>Description</th>
-            @can('get-brands-all-branches')
-            <th>Branch</th>
-            @endcan
-        </tr>
-    </thead>
+<div class="row">
+    <div class="col-md-12">
+        <table class="table table-bordered table-striped" id="brand_table">
+            <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Description</th>
+                    @can('get-brands-all-branches')
+                    <th>Branch</th>
+                    @endcan
+                </tr>
+            </thead>
 
-    <tbody>
-    @if($brands->count())
-    @foreach($brands as $brand)
-        <tr>
-            <td>
-                <a href="{{ route('brands.show', compact('brand')) }}">{{ $brand->name }}</a>
-            </td>
-            <td>{{ $brand->description ?? '-' }}</td>
-            @can('get-brands-all-branches')
-            <td>{{ $brand->branch->name }}</td>
-            @endcan
-        </tr>
-    @endforeach
-    @else
-        <tr>
-            @can('get-brands-all-branches')
-            <td colspan="3">No Data</td>
-            @elsecan
-            <td colspan="2">No Data</td>
-            @endcan
-        </tr>
-    @endif
-    </tbody>
-</table>
+            <tbody>
+            @if($brands->count())
+            @foreach($brands as $brand)
+                <tr>
+                    <td>
+                        <a href="{{ route('brands.show', compact('brand')) }}">{{ $brand->name }}</a>
+                    </td>
+                    <td>{{ $brand->description ?? '-' }}</td>
+                    @can('get-brands-all-branches')
+                    <td>{{ $brand->branch->name }}</td>
+                    @endcan
+                </tr>
+            @endforeach
+            @else
+                <tr>
+                    @can('get-brands-all-branches')
+                    <td colspan="3">No Data</td>
+                    @elsecan
+                    <td colspan="2">No Data</td>
+                    @endcan
+                </tr>
+            @endif
+            </tbody>
+        </table>
+    </div>
+</div>
 @endsection
 
-@section('scripts')
-@parent
+@push('scripts')
 @include('datatables')
 
 <script>
@@ -56,4 +55,4 @@ $(function() {
     $("#brand_table").DataTable();
 });
 </script>
-@endsection
+@endpush
