59a6b1fa3f6ad7ae3ea49ca6bc57c99eb1f103a4
Aiman Daniel <aiman2301@gmail.com>
2019-04-02 15:23:38 +0800

    Rewrite backend layout

--- resources/views/backend-layout.blade.php
+++ resources/views/backend-layout.blade.php
@@ -1,36 +1,41 @@
-@extends('layout')
+@push('stylesheets')
+<link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+@endpush
 
-@php
-    $parse_body_tag = false;
-@endphp
+@push('scripts')
+<script src="{{ mix('js/backend.js') }}"></script>
+@endpush
 
-@section('content')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title') - {{ config('app.name') }}</title>
+    @stack('stylesheets')
+</head>
+
 <body class="nav-md">
     <div class="container body">
         <div class="main_container">
-
             @include('sidebar')
             @include('navbar')
 
             <div class="right_col" role="main">
                 <div class="">
                     <div class="clearfix"></div>
-            
+
                     <div class="row">
                         <div class="col-md-12 col-sm-12 col-xs-12">
-                            @yield('content-1')
+                            @yield('content')
                         </div> <!-- end column -->
                     </div> <!-- end .row -->
                 </div>
             </div> <!-- end .right_col -->
-                
-            @yield('scripts-1')
-
         </div> <!-- end .main_container -->
     </div> <!-- end .container .body -->
 </body>
-@endsection
 
-@section('scripts')
-<script src="{{ mix('js/backend.js') }}"></script>
-@endsection
+@stack('scripts')
+</html>
