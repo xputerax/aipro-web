7b0a2e0b0a4b28b8da3bc94490ffbd22006cbde6
Aiman Daniel <aiman2301@gmail.com>
2019-04-02 18:17:54 +0800

    Remove junk, follow new template

--- resources/views/product/form.blade.php
+++ resources/views/product/form.blade.php
@@ -8,139 +8,133 @@
     @section('breadcrumbs', Breadcrumbs::render('product-create'))
 @endif
 
-@php
-    $parse_body_tag = false;
-@endphp
-
-@section('content-1')
+@section('content')
 @yield('breadcrumbs')
 
-<div class="x_panel">
-    <div class="x_content">
-        
-    @if(isset($product))
-    <form action="{{ route('products.update', compact('product')) }}" class="form-horizontal form-label-left" method="post">
-    @method('PUT')
-    @else
-    <form action="{{ route('products.store') }}" class="form-horizontal form-label-left" method="post">
-    @endif
-    
-        @csrf
+@if(isset($product))
+<form action="{{ route('products.update', compact('product')) }}" class="form-horizontal form-label-left" method="post">
+@method('PUT')
+@else
+<form action="{{ route('products.store') }}" class="form-horizontal form-label-left" method="post">
+@endif
 
-        <!-- start name section-->
-        <div class="form-group">
-            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">
-                Name <span class="required">*</span>
-            </label>
+    @csrf
 
-            <div class="col-md-6 col-sm-6 col-xs-12">
-                <input type="text" name="name" value="@if(isset($product)){{ $product->name }}@endif" class="form-control col-md-7 col-xs-12" required>
-            </div>
+    <!-- start name section-->
+    <div class="form-group">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">
+            Name <span class="required">*</span>
+        </label>
+
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <input type="text" name="name" value="@if(isset($product)){{ $product->name }}@endif" class="form-control col-md-7 col-xs-12" required>
         </div>
-        <!-- end name section -->
-
-        <!-- start description section-->
-        <div class="form-group">
-            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="description">
-                Description
-            </label>
-
-            <div class="col-md-6 col-sm-6 col-xs-12">
-                <textarea name="description" cols="30" rows="10" class="form-control col-md-7 col-xs-12">@if(isset($product)){{ $product->description }}@endif</textarea>
-            </div>
-        </div>
-        <!-- end description section -->
-
-        <!-- start brand section-->
-        <div class="form-group">
-            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="brand">
-                Brand <span class="required">*</span>
-            </label>
-
-            <div class="col-md-6 col-sm-6 col-xs-12">
-            @if($brands->count())
-                <select name="brand_id" class="select2 form-control" id="brand_select">
-                @foreach($brands as $brand)
-                    <option value="{{ $brand->id }}"{{ (isset($product) && $product->brand->id == $brand->id) ? ' selected' : '' }}>{{ $brand->name }}</option>
-                @endforeach
-                </select>
-            @else
-                No brand available
-            @endif
-            </div>
-        </div>
-        <!-- end description section -->
-
-        <!-- start min price section-->
-        <div class="form-group">
-            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="min_price">
-                Minimum Price <span class="required">*</span>
-            </label>
-
-            <div class="col-md-6 col-sm-6 col-xs-12">
-                <input type="text" name="min_price" value="@if(isset($product)){{ $product->min_price }}@endif" class="form-control col-md-7 col-xs-12" required>
-            </div>
-        </div>
-        <!-- end min price section -->
-
-        <!-- start max price section-->
-        <div class="form-group">
-            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="max_price">
-                Maximum Price <span class="required">*</span>
-            </label>
-
-            <div class="col-md-6 col-sm-6 col-xs-12">
-                <input type="text" name="max_price" value="@if(isset($product)){{ $product->max_price }}@endif" class="form-control col-md-7 col-xs-12" required>
-            </div>
-        </div>
-        <!-- end max price section -->
-
-        <!-- start stock section-->
-        <div class="form-group">
-            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="stock">
-                Stock <span class="required">*</span>
-            </label>
-
-            <div class="col-md-6 col-sm-6 col-xs-12">
-                <input type="text" name="stock" value="@if(isset($product)){{ $product->stock }}@endif" class="form-control col-md-7 col-xs-12" required>
-            </div>
-        </div>
-        <!-- end stock section -->
-
-        <div class="form-group">
-            <label class="control-label col-md-3 col-sm-3 col-xs-12" for="Type">
-                Type <span class="required">*</span>
-            </label>
-
-            <div class="col-md-6 col-sm-6 col-xs-12">
-                <select name="type" class="form-control">
-                    <option value="product" {{ isset($product) && $product->type == 'product' ? 'selected' : '' }}>Product</option>
-                    <option value="service" {{ isset($product) && $product->type == 'service' ? 'selected' : '' }}>Service</option>
-                </select>
-            </div>
-        </div>
-
-        <!-- start button section-->
-        <div class="form-group">
-            <div class="col-md-offset-3 col-sm-offset-3 col-md-6 col-sm-6 col-xs-12">
-                <input type="submit" value="{{ isset($product) ? 'Save' : 'Add Product'}}" class="btn btn-primary">
-            </div>
-        </div>
-        <!-- end button section -->
-
-        </form>
-
-        @if(session('message'))
-        {{ session('message') }}
-        @endif
-
-        @if($errors->any())
-        @foreach($errors->all() as $error)
-        {{ $error }}
-        @endforeach
-        @endif
     </div>
-</div>
+    <!-- end name section -->
+
+    <!-- start description section-->
+    <div class="form-group">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="description">
+            Description
+        </label>
+
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <textarea name="description" cols="30" rows="10" class="form-control col-md-7 col-xs-12">@if(isset($product)){{ $product->description }}@endif</textarea>
+        </div>
+    </div>
+    <!-- end description section -->
+
+    <!-- start brand section-->
+    <div class="form-group">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="brand">
+            Brand <span class="required">*</span>
+        </label>
+
+        <div class="col-md-6 col-sm-6 col-xs-12">
+        @if($brands->count())
+            <select name="brand_id" class="select2 form-control" id="brand_select">
+            @foreach($brands as $brand)
+                <option value="{{ $brand->id }}"
+                    {{ (isset($product) && $product->brand->id == $brand->id) ? ' selected' : '' }}>
+                {{ $brand->name }}</option>
+            @endforeach
+            </select>
+        @else
+            No brand available
+        @endif
+        </div>
+    </div>
+    <!-- end description section -->
+
+    <!-- start min price section-->
+    <div class="form-group">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="min_price">
+            Minimum Price <span class="required">*</span>
+        </label>
+
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <input type="text" name="min_price" value="@if(isset($product)){{ $product->min_price }}@endif" class="form-control col-md-7 col-xs-12" required>
+        </div>
+    </div>
+    <!-- end min price section -->
+
+    <!-- start max price section-->
+    <div class="form-group">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="max_price">
+            Maximum Price <span class="required">*</span>
+        </label>
+
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <input type="text" name="max_price" value="@if(isset($product)){{ $product->max_price }}@endif" class="form-control col-md-7 col-xs-12" required>
+        </div>
+    </div>
+    <!-- end max price section -->
+
+    <!-- start stock section-->
+    <div class="form-group">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="stock">
+            Stock <span class="required">*</span>
+        </label>
+
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <input type="text" name="stock" value="@if(isset($product)){{ $product->stock }}@endif"
+                class="form-control col-md-7 col-xs-12" required>
+        </div>
+    </div>
+    <!-- end stock section -->
+
+    <div class="form-group">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="Type">
+            Type <span class="required">*</span>
+        </label>
+
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <select name="type" class="form-control">
+                <option value="product" {{ isset($product) && $product->type == 'product' ? 'selected' : '' }}>Product</option>
+                <option value="service" {{ isset($product) && $product->type == 'service' ? 'selected' : '' }}>Service</option>
+            </select>
+        </div>
+    </div>
+
+    <!-- start button section-->
+    <div class="form-group">
+        <div class="col-md-offset-3 col-sm-offset-3 col-md-6 col-sm-6 col-xs-12">
+            <input type="submit" value="{{ isset($product) ? 'Save' : 'Add Product'}}" class="btn btn-primary">
+        </div>
+    </div>
+    <!-- end button section -->
+
+    </form>
+
+    @if(session('message'))
+    {{ session('message') }}
+    @endif
+
+    @if($errors->any())
+    @foreach($errors->all() as $error)
+    {{ $error }}
+    @endforeach
+    @endif
 @endsection
 
 @section('scripts')
