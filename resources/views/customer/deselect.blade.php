11de963096c3178ade6b6ba1d3d76e7b779315be
Aiman Daniel <aiman2301@gmail.com>
2019-04-02 15:54:03 +0800

    Add row and col

--- resources/views/customer/deselect.blade.php
+++ resources/views/customer/deselect.blade.php
@@ -6,14 +6,18 @@
 @section('content')
 @yield('breadcrumbs')
 
-@if(isset($customer))
-<form action="{{ route('customers.deselect') }}" method="get">
-    <h4>Deselect customer {{ $customer->full_name }}</h4>
-    <input type="submit" value="Confirm Deselect" class="btn btn-primary">
-</form>
-@else
-<h4>
-    No customer selected!
-</h4>
+<div class="row">
+    <div class="col-md-12">
+        @if(isset($customer))
+        <form action="{{ route('customers.deselect') }}" method="get">
+            <h4>Deselect customer {{ $customer->full_name }}</h4>
+            <input type="submit" value="Confirm Deselect" class="btn btn-primary">
+        </form>
+        @else
+        <h4>
+            No customer selected!
+        </h4>            
+    </div>
+</div>
 @endif
 @endsection
