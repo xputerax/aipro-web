f2558da41872798e03b3378e31534d9d62115ad4
Aiman Daniel <aiman2301@gmail.com>
2019-04-02 15:56:06 +0800

    Remove junk, add row & col, change content-1 to content

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -1,51 +1,52 @@
 @extends('backend-layout')
 
 @section('title', 'Order List')
+@section('breadcrumbs', Breadcrumbs::render('order-'.$status))
 
-@php
-    $parse_body_tag = false;
-@endphp
-
-@section('breadcrumbs')
-{{ Breadcrumbs::render('order-'.$status) }}
-@endsection
-
-@section('content-1')
+@section('content')
 @yield('breadcrumbs')
 
-<table class="table table-bordered table-striped" id="orders_table">
-    <thead>
-        <tr>
-            <th>Customer</th>
-            <th>Status</th>
-            <th>Date</th>
-            <th>Action</th>
-        </tr>
-    </thead>
+<div class="row">
+    <div class="col-md-12">
+        <table class="table table-bordered table-striped" id="orders_table">
+            <thead>
+                <tr>
+                    <th>Customer</th>
+                    <th>Status</th>
+                    <th>Date</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
 
-    <tbody>
-    @if($orders->count())
-        @foreach($orders as $order)
-        <tr>
-            <td>
-                <a href="{{ route('orders.show', compact('order')) }}">
-                    {{ $order->customer->full_name }}
-                </a>
-            </td>
-            <td>{{ $order->status }}</td>
-            <td>{{ $order->created_at }}</td>
-            <td>
-                <a href="{{ route('orders.edit', compact('order')) }}" class="btn btn-primary">Edit</a>
-            </td>
-        </tr>
-        @endforeach
-    @else
-        <tr>
-            <td colspan="4">No data</td>
-        </tr>
-    @endif
-    </tbody>
-</table>
+            <tbody>
+            @if($orders->count())
+                @foreach($orders as $order)
+                <tr>
+                    <td>
+                        <a href="{{ route('orders.show', compact('order')) }}">
+                            {{ $order->customer->full_name }}
+                        </a>
+                    </td>
+                    <td>{{ $order->status }}</td>
+                    <td>{{ $order->created_at }}</td>
+                    <td>
+                        <a href="{{ route('orders.edit', compact('order')) }}" class="btn btn-primary">Edit</a>
+                    </td>
+                </tr>
+                @endforeach
+            @else
+                <tr>
+                    <td colspan="4">No data</td>
+                </tr>
+            @endif
+            </tbody>
+        </table>
+    </div>
+</div>
 
-{{ $orders->links() }}
+<div class="row">
+    <div class="col-md-12">
+        {{ $orders->links() }}
+    </div>
+</div>
 @endsection
