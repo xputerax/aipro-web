4911be62092326c3d2cd5125b42a9c4405314cfc
Aiman Daniel <aiman2301@gmail.com>
2019-04-03 14:38:53 +0800

    Minor changes
    - Remove cannot_get_sales_report_all_branches callback
    - Change where clause to own branch id/selected branch id

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Carbon;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
@@ -13,44 +14,35 @@ class DashboardController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
-    public function index()
+    public function index(Request $request)
     {
         $user = Auth::user();
 
         $todays_date = Carbon::now();
-
-        $cannot_get_sales_report_all_branches = function ($query) use ($user) {
-            if ($user->cannot('get-sales-report-all-branches')) {
-                $query->where('branch_id', $user->branch_id);
-            }
-        };
 
         $sales_today = DB::table('order_payments')
             ->selectRaw('COALESCE(SUM(amount), "0.00") as sum_sales')
             ->whereRaw('DATE(created_at) = ?', [
                 $todays_date->toDateString(),
             ])
-            ->where($cannot_get_sales_report_all_branches)
-            ->first()
-        ;
+            ->where('branch_id', $request->session()->get('selected_branch_id', $user->branch_id))
+            ->first();
 
         $sales_month = DB::table('order_payments')
             ->selectRaw('COALESCE(SUM(amount), "0.00") as sum_sales')
             ->whereRaw('DATE_FORMAT(created_at, "%Y-%m") = ?', [
                 $todays_date->format('Y-m'),
             ])
-            ->where($cannot_get_sales_report_all_branches)
-            ->first()
-        ;
+            ->where('branch_id', $request->session()->get('selected_branch_id', $user->branch_id))
+            ->first();
 
         $sales_year = DB::table('order_payments')
             ->selectRaw('COALESCE(SUM(amount), "0.00") as sum_sales')
             ->whereRaw('YEAR(created_at) = ?', [
                 $todays_date->format('Y'),
             ])
-            ->where($cannot_get_sales_report_all_branches)
-            ->first()
-        ;
+            ->where('branch_id', $request->session()->get('selected_branch_id', $user->branch_id))
+            ->first();
 
         $sales = [
             'day' => $sales_today->sum_sales,
