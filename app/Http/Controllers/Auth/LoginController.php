a62fd484201a77a5b5a5aa08cbc16bcc26a1ee2e
Aiman Daniel <aiman2301@gmail.com>
2019-04-04 13:14:48 +0800

    Put default value for selected_branch_id session

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
 
 class LoginController extends Controller
 {
@@ -40,5 +41,10 @@ class LoginController extends Controller
     public function username()
     {
         return 'username';
+    }
+
+    protected function authenticated(Request $request, $user)
+    {
+        $request->session()->put('selected_branch_id', $user->branch_id);
     }
 }
