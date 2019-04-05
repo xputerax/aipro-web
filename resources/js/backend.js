0418ddcd3653272ecf97eea3def945f42f219253
Aiman Daniel <aiman2301@gmail.com>
2019-04-05 11:41:57 +0800

    Rewrite backend javascript to include datatables

--- resources/js/backend.js
+++ resources/js/backend.js
@@ -1,47 +1,20 @@
-try {
-    window.Popper = require('popper.js').default;
-    window.$ = window.jQuery = require('jquery');
-    // window.PNotify = require('pnotify');
+var $ = window.$ = window.jQuery = require('jquery');
+var DataTable = require('datatables.net');
 
-    window.PNotify = require('pnotify/dist/pnotify')
-    require('pnotify/dist/pnotify.buttons')
-    require('pnotify/dist/pnotify.nonblock')
-    require('bootstrap');
-    // window.$.fn.DataTable = require('datatables.net/js/jquery.dataTables');
-    // window.$.fn.DataTable = require('datatables.net-bs/js/dataTables.bootstrap');
+require('datatables.net-bs');
+require( 'datatables.net-buttons' );
+require( 'datatables.net-responsive' );
+require('bootstrap');
+require('../gentelella/js/custom');
+require('../gentelella/js/helpers/smartresize');
+require('select2');
 
-    // require('datatables.net');
-    // window.datatables = require('datatables.net-bs');
+$.fn.dataTable = DataTable;
+$.fn.dataTableSettings = DataTable.settings;
+$.fn.dataTableExt = DataTable.ext;
 
-    // require('fastclick/lib/fastclick');
-    // require('nprogress/nprogress');
-    // require('icheck/icheck');
-    // require('pnotify/src/pnotify');
-    // require('pnotify/src/pnotify.buttons');
-    // require('pnotify/src/pnotify.nonblock');
-    require('../gentelella/js/custom');
+DataTable.$ = $;
 
-    // require('bootstrap-progressbar/bootstrap-progressbar');
-    require('../gentelella/js/helpers/smartresize');
-
-    // require('datatables.net-bs/js/dataTables.bootstrap');
-    // require('datatables.net-buttons/js/dataTables.buttons');
-    // require('datatables.net-buttons/js/buttons.flash');
-    // require('datatables.net-buttons/js/buttons.html5');
-    // require('datatables.net-buttons/js/buttons.print');
-    // require('datatables.net-buttons-bs/js/buttons.bootstrap');
-    // require('datatables.net-fixedheader/js/dataTables.fixedHeader');
-    // require('datatables.net-keytable/js/dataTables.keyTable');
-    // require('datatables.net-responsive/js/dataTables.responsive');
-    // require('datatables.net-responsive-bs/js/responsive.bootstrap');
-    // require('datatables.net-scroller/js/dataTables.scroller');
-
-    // require('jszip/dist/jszip');
-    // require('pdfmake/build/pdfmake');
-    // require('pdfmake/build/vfs_fonts');
-
-    require('select2');
-
-} catch (e) {
-
-}
+$.fn.DataTable = function ( opts ) {
+  return $(this).dataTable( opts ).api();
+};
