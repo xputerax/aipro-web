e1fbd383c338e3d3827d3b21ba5f951c156c8b2f
Aiman Daniel <aiman2301@gmail.com>
2019-04-06 17:37:27 +0800

    Rewrite user factory

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -14,11 +14,14 @@ use Faker\Generator as Faker;
 */
 
 $factory->define(App\User::class, function (Faker $faker) {
+    $faker->addProvider(new \Faker\Provider\ms_MY\Person($faker));
+
     return [
-        // 'name' => $faker->name,
-        // 'email' => $faker->unique()->safeEmail,
-        // 'email_verified_at' => now(),
-        // 'password' => '$2y$10$TKh8H1.PfQx37YgCzwiKb.KjNyWgaHb9cbcoQgdIVFlYg7B77UdFm', // secret
-        // 'remember_token' => str_random(10),
+        'email' => $faker->safeEmail,
+        'username' => $faker->userName,
+        'full_name' => $faker->name,
+        'password' => '123456',
+        'group_id' => 3,
+        'branch_id' => 1,
     ];
 });
