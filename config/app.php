d3e242bf470b5735c0a4c32990d99517add0115c
Aiman Daniel <aiman2301@gmail.com>
2019-04-06 18:36:08 +0800

    Minor fix - change to real facade class

--- config/app.php
+++ config/app.php
@@ -230,7 +230,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'DataTables' => Yajra\DataTables\Facades\DataTables::class,
         'PDF' => Barryvdh\DomPDF\Facade::class,
-        'Breadcrumbs' => DaveJamesMiller\Breadcrumbs\BreadcrumbsManager::class
+        'Breadcrumbs' => DaveJamesMiller\Breadcrumbs\Facades\Breadcrumbs::class
 
     ],
 
