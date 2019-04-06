ab74949925c5446b67588676e5f441ac2f41d673
Aiman Daniel <aiman2301@gmail.com>
2019-04-06 17:36:38 +0800

    Branch and user is now random

--- database/factories/CustomerFactory.php
+++ database/factories/CustomerFactory.php
@@ -4,20 +4,22 @@ use Faker\Generator as Faker;
 use Illuminate\Support\Carbon;
 
 $factory->define(App\Customer::class, function (Faker $faker) {
-
     $faker->addProvider(new \Faker\Provider\ms_MY\PhoneNumber($faker));
     $faker->addProvider(new \Faker\Provider\ms_MY\Person($faker));
 
     $sex = $faker->randomElement(['male', 'female']);
     $source = $faker->randomElement(['friend', 'google', 'facebook', 'advertisement']);
 
+    $branch = \App\Branch::orderByRaw('RAND()')->first();
+    $user = \App\User::where('branch_id', $branch->id)->orderByRaw('RAND()')->first();
+
     return [
         'full_name' => $faker->name,
         'phone' => $faker->mobileNumber(false, false),
         'ic_number' => $faker->myKadNumber($sex, false),
         'sex' => $sex,
-        'branch_id' => 1,
-        'user_id' => 1,
+        'branch_id' => $branch->id,
+        'user_id' => $user->id,
         'source' => $source,
         'created_at' => Carbon::now(),
     ];
