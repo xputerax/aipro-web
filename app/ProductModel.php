509ec1f2c31bf5e8ac9239338b8785a368fd2d00
Aiman Daniel <aiman2301@gmail.com>
2019-04-15 15:53:44 +0800

    Add ProductModel model

---
+++ app/ProductModel.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ProductModel extends Model
+{
+    protected $table = 'product_models';
+
+    protected $fillable = [
+        'branch_id', 'brand_id', 'name', 'description', 'created_at', 'updated_at'
+    ];
+
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+}
