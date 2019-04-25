c57d644926f37b07fd6b5ccfba97d71e5e6c2cf3
Aiman Daniel <aiman2301@gmail.com>
2019-04-25 16:44:44 +0800

    Add ajax, add styling to messages

--- resources/views/product/form.blade.php
+++ resources/views/product/form.blade.php
@@ -20,6 +20,40 @@
 
     @csrf
 
+    <!-- start brand section-->
+    <div class="form-group">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="brand">
+            Brand <span class="required">*</span>
+        </label>
+
+        <div class="col-md-6 col-sm-6 col-xs-12">
+        @if($brands->count())
+            <select name="brand_id" class="select2 form-control" id="brand_select">
+            @foreach($brands as $brand)
+                <option value="{{ $brand->id }}"
+                    {{ (isset($product) && $product->brand->id == $brand->id) ? ' selected' : '' }}>{{ $brand->name }}</option>
+            @endforeach
+            </select>
+        @else
+            No brand available
+        @endif
+        </div>
+    </div>
+    <!-- end description section -->
+
+    <!-- start model section-->
+    <div class="form-group">
+        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="model">
+            Model <span class="required">*</span>
+        </label>
+
+        <div class="col-md-6 col-sm-6 col-xs-12">
+            <select name="model_id" id="model_select" class="form-control">
+            </select>
+        </div>
+    </div>
+    <!-- end model section -->
+
     <!-- start name section-->
     <div class="form-group">
         <label class="control-label col-md-3 col-sm-3 col-xs-12" for="name">
@@ -40,28 +74,6 @@
 
         <div class="col-md-6 col-sm-6 col-xs-12">
             <textarea name="description" cols="30" rows="10" class="form-control col-md-7 col-xs-12">@if(isset($product)){{ $product->description }}@endif</textarea>
-        </div>
-    </div>
-    <!-- end description section -->
-
-    <!-- start brand section-->
-    <div class="form-group">
-        <label class="control-label col-md-3 col-sm-3 col-xs-12" for="brand">
-            Brand <span class="required">*</span>
-        </label>
-
-        <div class="col-md-6 col-sm-6 col-xs-12">
-        @if($brands->count())
-            <select name="brand_id" class="select2 form-control" id="brand_select">
-            @foreach($brands as $brand)
-                <option value="{{ $brand->id }}"
-                    {{ (isset($product) && $product->brand->id == $brand->id) ? ' selected' : '' }}>
-                {{ $brand->name }}</option>
-            @endforeach
-            </select>
-        @else
-            No brand available
-        @endif
         </div>
     </div>
     <!-- end description section -->
@@ -127,12 +139,16 @@
     </form>
 
     @if(session('message'))
-    {{ session('message') }}
+    <div class="alert alert-info">
+        {{ session('message') }}
+    </div>
     @endif
 
     @if($errors->any())
     @foreach($errors->all() as $error)
-    {{ $error }}
+    <div class="alert alert-danger">
+        {{ $error }}
+    </div>
     @endforeach
     @endif
 @endsection
@@ -142,7 +158,60 @@
 
 <script>
 $(function() {
-    $("#brand_select").select2();
+
+    var current_model_id = "{{ isset($product) ? $product->model_id : 0 }}";
+
+    var refresh_model_list = function () {
+
+        var selected_brand = $("#brand_select > option:selected");
+
+        $.ajax({
+            url: "{{ route('api.product_models.index') }}",
+            data: {
+                columns: [
+                    {
+                        data: 'brand.name',
+                        name: '',
+                        searchable: true,
+                        orderable: true,
+                        search: {
+                            value: selected_brand.text(),
+                            regex: false
+                        }
+                    },
+                ],
+            },
+            success: function (response, status) {
+                let models = response.data;
+
+                if (models.length > 0) {
+                    models.forEach(function (model) {
+                        let modelOption = document.createElement('option');
+                        modelOption.value = model.id;
+                        modelOption.text = model.name;
+                        modelOption.selected = model.id == current_model_id;
+
+                        $("#model_select").append(modelOption);
+                    });
+                } else {
+                    let noModelOption = document.createElement('option');
+                    noModelOption.text = 'No model exist for this brand';
+                    noModelOption.selected = true;
+
+                    $("#model_select").append(noModelOption).attr('disabled', true);
+                }
+            }
+        });
+
+    };
+
+    refresh_model_list();
+
+    $("#brand_select").on('change', function(e) {
+        $("#model_select").removeAttr('disabled').children().remove();
+        refresh_model_list();
+    });
+
 });
 </script>
 @endsection
